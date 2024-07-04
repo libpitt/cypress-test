73a70aa2d84dffa94346b082f80f2e6d2381f7d7
Lisa-Ann B <lisabruney@pitt.edu>
2024-07-04 16:17:35 -0400

    Update printer

--- ci/main.py
+++ ci/main.py
@@ -4,6 +4,32 @@ import json
 from websockets.sync.client import connect
 import datetime
 
+
+def workflow_printer(message):
+    result = json.loads(message)
+    if is_complete(result):
+        print(f"::notice title=Tests complete")
+        return
+    test = result.get('report')
+    if test is not None and test.get('reporter') == 'spec':
+        print(f"::group::{test.get('spec').get('relative')}")  # group title
+        stats = test.get('reporterStats', '')
+        state = 'error' if stats.get('failures') > 0 else 'notice'
+        print(f"::{state} title=Tests: {stats.get('tests')} ; Passes: {stats.get('passes')} ; Fails: {stats.get('failures')}")
+        for t in test.get('tests'):
+            state = 'error' if t.get('state') != 'passed' else 'notice'
+            title = ' > '.join(t.get('title', ''))
+            print(f"::{state} title={title}")
+        print("::endgroup::")
+    else:
+        print(f"::notice title={json.dumps(result)}")
+
+
+def is_complete(result):
+    path = result.get('path')
+    return path is not None and 'complete.json' in path
+
+
 def main():
     with connect("wss://c81f-2601-547-cc01-6200-8df3-bd33-eb87-696f.ngrok-free.app") as websocket:
         websocket.send(f"Requesting Cypress reports ... Date: {datetime.datetime.now()}. ")
@@ -11,12 +37,11 @@ def main():
         results = {}
         while receiving is True:
             message = websocket.recv()
+            workflow_printer(message)
             result = json.loads(message)
-            results.update(result)
-            print(f"Received: {message}")
-            path = result.get('path')
-            if path is not None and 'complete.json' in result.get('path'):
+            if is_complete(result):
                 receiving = False
+            results.update(result)
 
         return json.dumps(results)
 
